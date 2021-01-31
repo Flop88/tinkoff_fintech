b8235fa4d1557f5d6ae807e76dedea69605814bc
Flop88 <wwwqwwwqwwwqwwwq@gmail.com>
2021-01-31 23:02:21 +0300

    Add check internet connection

--- app/src/main/java/ru/mvlikhachev/tinkoff_fintech/MainActivity.kt
+++ app/src/main/java/ru/mvlikhachev/tinkoff_fintech/MainActivity.kt
@@ -1,17 +1,24 @@
 package ru.mvlikhachev.tinkoff_fintech
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.os.Bundle
-import android.util.Log
-import androidx.lifecycle.Observer
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
 import kotlinx.android.synthetic.main.activity_main.*
+import ru.mvlikhachev.tinkoff_fintech.model.Model
 import ru.mvlikhachev.tinkoff_fintech.repository.Repository
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
+
+    private lateinit var cacheModel : ArrayList<Model>
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +27,13 @@ class MainActivity : AppCompatActivity() {
 
         initialization()
 
+        previousButton.setOnClickListener {
+            loadPreviousImage()
+        }
 
+        nextButton.setOnClickListener {
+            loadNextImage()
+        }
 
 
     }
@@ -28,25 +41,46 @@ class MainActivity : AppCompatActivity() {
     private fun initialization() {
         val repository = Repository()
         val viewModelFactory = MainViewModelFactory(repository)
+        cacheModel = ArrayList()
         viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
-        viewModel.getModel()
-        loadImage()
+
+        loadNextImage()
     }
 
-    private fun loadImage() {
-        viewModel.myResponse.observe(this, { response ->
-            if (response.isSuccessful) {
-                val media = response.body()?.gifURL.toString()
-                if (media !== null) {
-                    Glide.with(this)
-                            .load(media)
-                            .into(loadImageView)
-                } else {
-                    loadImageView.setImageResource(R.drawable.ic_launcher_background)
+    private fun loadNextImage() {
+
+        if (isNetworkAvailable()) {
+            viewModel.getModel()
+            viewModel.myResponse.observe(this, { response ->
+                val requestOptions = RequestOptions()
+                        .diskCacheStrategy(DiskCacheStrategy.ALL)
+
+                if (response.isSuccessful) {
+                    val media = response.body()?.gifURL.toString()
+                    if (media !== null) {
+                        Glide.with(this)
+                                .load(media)
+                                .apply(requestOptions)
+                                .into(loadImageView)
+                    }
                 }
-            }
-        })
+            })
+        } else {
+            loadImageView.setImageResource(R.drawable.ic_baseline_signal_cellular_off_24)
+        }
     }
 
+
+    private fun loadPreviousImage() {
+
+        }
+
+    private fun isNetworkAvailable(): Boolean {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE)
+        return if (connectivityManager is ConnectivityManager) {
+            val networkInfo: NetworkInfo? = connectivityManager.activeNetworkInfo
+            networkInfo?.isConnected ?: false
+        } else false
+    }
 
 }
